a2d513fa28cfb5a11ee6a541b8e5ec518d2f93ab
Harsh Singh <harsh.101783@stu.upes.ac.in>
2024-02-16 12:21:11 +0530

    started printing pattern

--- pattern_printing.c
+++ pattern_printing.c
@@ -3,19 +3,27 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
 
-    int n;
-
-    scanf("%d", &n);
-  	int i,j;
-    for(i=0;i<n*2-1;i++){
-        for (j= 0; j < n*2-1; j++)
-        {
-           
-        }
-         printf("%d %d %d\n",n,n,n);
+  int n;
+  scanf("%d", &n);
+  int a[n][n];
+  int i, j;
+  for (i = 0; i < n * 2 - 1; i++)
+  {
+    for (j = 0; j < n * 2 - 1; j++)
+    {
+      a[i][j]=0;
     }
-    return 0;
+  }
+   for (i = 0; i < n * 2 - 1; i++)
+  {
+    for (j = 0; j < n * 2 - 1; j++)
+    {
+      printf("%d\t",a[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
 }
