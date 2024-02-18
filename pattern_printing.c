2b92cc0fbd89dd0117765832fb1e534488e9aa0b
Harsh Singh <harsh.101783@stu.upes.ac.in>
2024-02-18 14:57:48 +0530

    Printing pattern completed 7 more to go

--- pattern_printing.c
+++ pattern_printing.c
@@ -23,15 +23,18 @@ int main()
     }
 
     // Update specific elements of the array
+    int temp=n;
     for (int i = 1; i < n; i++)
     {
         for (int j = i; j < 2 * n - i-1; j++)
         {
-            a[i][j] = n - 1;
-            a[j][i] = n - 1;
-            a[j][2 * n - i - 2] = n - 1;
-            a[2 * n - i - 2][j] = n - 1;
+            
+            a[i][j] = temp - 1;
+            a[j][i] = temp - 1;
+            a[j][2 * n - i - 2] = temp - 1;
+            a[2 * n - i - 2][j] = temp - 1;
         }
+        temp=temp-1;
     }
 
     // Print the array
