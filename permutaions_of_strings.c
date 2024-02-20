ce4fd5ff78da98de706e1a8d8d2e7bfef7c10cd2
Harsh Singh <harsh.101783@stu.upes.ac.in>
2024-02-21 00:01:50 +0530

    5 more to go

--- permutaions_of_strings.c
+++ permutaions_of_strings.c
@@ -4,34 +4,48 @@
 
 int next_permutation(int n, char **s)
 {
-    int i, j, k;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            
-        }
+    if(n==1){
+        return 0;
     }
+    else{
+        for(int i=n-2;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+               if(strcmp(s[j],s[i])>0){
+                    char *temp=s[i];
+                    s[i]=s[j];
+                    s[j]=temp;
+                    for(int l=i+1,k=n-1;l<k;l++,k--){
+                        char *temp=s[l];
+                        s[l]=s[k];
+                        s[k]=temp;
+                    }
+                     return 1;
+                }
+            }
+        }
+       
+    }
+    return 0;
 }
 
 int main()
 {
-    char **s;
-    int n;
-    scanf("%d", &n);
-    s = calloc(n, sizeof(char *));
-    for (int i = 0; i < n; i++)
-    {
-        s[i] = calloc(11, sizeof(char));
-        scanf("%s", s[i]);
-    }
-    do
-    {
-        for (int i = 0; i < n; i++)
-            printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
-    } while (next_permutation(n, s));
-    for (int i = 0; i < n; i++)
-        free(s[i]);
-    free(s);
-    return 0;
+	char **s;
+	int n;
+	scanf("%d", &n);
+	s = calloc(n, sizeof(char*));
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = calloc(11, sizeof(char));
+		scanf("%s", s[i]);
+	}
+	do
+	{
+		for (int i = 0; i < n; i++)
+			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	} while (next_permutation(n, s));
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
+	return 0;
 }
