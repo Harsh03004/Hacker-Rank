8f926532d6b4dea9e8ee8517d9d16cfe58421dd9
Harsh Singh <harsh.101783@stu.upes.ac.in>
2024-02-20 22:47:21 +0530

    6 remaining

---
+++ permutaions_of_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int next_permutation(int n, char **s)
+{
+    int i, j, k;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+    char **s;
+    int n;
+    scanf("%d", &n);
+    s = calloc(n, sizeof(char *));
+    for (int i = 0; i < n; i++)
+    {
+        s[i] = calloc(11, sizeof(char));
+        scanf("%s", s[i]);
+    }
+    do
+    {
+        for (int i = 0; i < n; i++)
+            printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+    } while (next_permutation(n, s));
+    for (int i = 0; i < n; i++)
+        free(s[i]);
+    free(s);
+    return 0;
+}
