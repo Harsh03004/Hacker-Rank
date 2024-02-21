3e31533c31ce245e600c7f63cc2803316fa2ab6d
Harsh Singh <harsh.101783@stu.upes.ac.in>
2024-02-21 20:01:25 +0530

    6 more

--- triangle.c
+++ triangle.c
@@ -4,29 +4,43 @@
 
 struct triangle
 {
-	int a;
-	int b;
-	int c;
+    int a;
+    int b;
+    int c;
 };
 
 typedef struct triangle triangle;
-void sort_by_area(triangle* tr, int n) {
-	/**
-	* Sort an array a of the length n
-	*/
+void sort_by_area(triangle *tr, int n)
+{
+    float *s = (float *)malloc(n * sizeof(float));
+    float *area = (float *)malloc(n * sizeof(float));
+    for (int i = 0; i < n; i++)
+    {
+        s[i] = (tr[i].a + tr[i].b + tr[i].c) / 2.0;
+        area[i] = sqrt(s[i] * (s[i] - tr[i].a) * (s[i] - tr[i].b) * (s[i] - tr[i].c));
+    }
+    for(int i=0;i<n;i++){
+        if(area[i]>area[i+1] && area[i]>area[i+2]){
+           float temp = area[i];
+           area[i]=area[i+1];
+           area[i+1]=temp;
+        }
+    }
 }
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	triangle *tr = malloc(n * sizeof(triangle));
-	for (int i = 0; i < n; i++) {
-		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
-	}
-	sort_by_area(tr, n);
-	for (int i = 0; i < n; i++) {
-		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
-	}
-	return 0;
+    int n;
+    scanf("%d", &n);
+    triangle *tr = malloc(n * sizeof(triangle));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+    }
+    sort_by_area(tr, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+    }
+    return 0;
 }
